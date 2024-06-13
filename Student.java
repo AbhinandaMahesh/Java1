ea8536504188a504715d32342bb3fe95a246cc18
Abhinanda A M <abhiluckylad03@gmail.com>
2024-06-13 12:02:29 +0530

    Add files via upload

--- Student.java
+++ Student.java
@@ -1,46 +1,59 @@
-////Program to demonstrate Hierarchical Inheritance - Student Class
-package com.tnsif.dayfive.hierarchicalinheritance;
+//Program to define a class implementing the RegisteredStudent marker interface
+package com.tnsif.dayeight.interfaces.markerinterfaces;
 
-public class Student extends Person
-{
-	private String clas;
-	private float per;
-	public Student() {
-		System.out.println("Student class default constructor");
-		clas="FY";
-		per=70;
+public class Student implements Registrable {
+	private int id;
+	private String name;
+	private double fees;
+	private String course;
+
+	Student() {
+		super();
 	}
-	public Student(String clas, float per) {
-		this.clas = clas;
-		this.per = per;
+
+	public Student(int id, String name, double fees, String course) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.fees = fees;
+		this.course = course;
 	}
-	
-	public String getClas() {
-		return clas;
+
+	public int getId() {
+		return id;
 	}
-	public void setClas(String clas) {
-		this.clas = clas;
+
+	public void setId(int id) {
+		this.id = id;
 	}
-	public float getPer() {
-		return per;
+
+	public String getName() {
+		return name;
 	}
-	public void setPer(float per) {
-		this.per = per;
+
+	public void setName(String name) {
+		this.name = name;
 	}
-	public Student(String name, String city,String clas, float per )
-	{
-		//private members can't inherited into child class
-		/*
-		 * super.name=name; super.city=city;
-		 */
-		super(name,city);
-		this.clas = clas;
-		this.per = per;
+
+	public double getFees() {
+		return fees;
 	}
+
+	public void setFees(double fees) {
+		this.fees = fees;
+	}
+
+	public String getCourse() {
+		return course;
+	}
+
+	public void setCourse(String course) {
+		this.course = course;
+	}
+
 	@Override
 	public String toString() {
-		return "Student [clas=" + clas + ", per=" + per + ", getName()=" + getName() + ", getCity()=" + getCity() + "]";
+		return "Student [id=" + id + ", name=" + name + ", fees=" + fees + ", course=" + course + "]";
 	}
-	
-	
+
 }
