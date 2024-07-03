7fad71485fee1af0e46f7a5dd09cf5750fe89442
Abhinanda A M <abhiluckylad03@gmail.com>
2024-07-03 18:25:26 +0530

    Add files via upload

---
+++ LinkedListDemo.java
@@ -0,0 +1,10 @@
+package com.tnsif.collection;
+
+public class LinkedListDemo {
+
+	public static void main(String[] args) {
+		// add(); addFirst(); addLast(); getFirst(); getLast(); removeFirst(); removeLast(); sort(); reverse();
+
+	}
+
+}
